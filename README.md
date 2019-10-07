60453cae14d2231734809050f3c01e38d5d1d722
Brian Leonard <brian.leonard@mongodb.com>
2019-10-07 14:57:27 -0400

    First draft

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # terraform-atlas
-Sample code to deploy MongoDB Atlas clusters using Terraform
+Sample code to deploy MongoDB Atlas clusters using the Terraform [MongoDB Atlas Provider](https://www.terraform.io/docs/providers/mongodbatlas/index.html)
+
+Clone this repository
+```
+$ git clone https://github.com/wbleonard/terraform-atlas.git
+```
+
+Add a `terraform.tfvars` file with your Atlas API key values:
+```
+mongodb_atlas_api_public_key = "gzwmxxxx"
+mongodb_atlas_api_private_key = "0393d504-xxxx-4bda-xxxx-5564430xxxxx"
+```
+ 
+Initialize the MongoDB Atlas Provider
+```
+$ terraform init
+```
+
+Apply
+```
+$ terraform apply
+```
+
+View
+```
+$ terraform show
+```
---
+++ gcp.tf
@@ -0,0 +1,72 @@
+# Variables we need for this Terraform run. Set them in variables.tf
+variable "api_public_key" { default="" }
+variable "api_private_key" { default="" }
+variable "org_id" { default="" }
+variable "project_name" { default="" }
+variable "database_username" { default = "" }
+variable "database_user_password" { default = "" }
+variable "whitelist_ip" { default = "" }
+variable "whitelist_ip_desc" { default = "" }
+
+#
+# Configure the MongoDB Atlas Provider
+#
+provider "mongodbatlas" {
+  public_key = var.api_public_key
+  private_key  = var.api_private_key
+}
+
+#
+# Create a Project 
+#
+resource "mongodbatlas_project" "my_project" {
+  name 			= var.project_name
+  org_id		= var.org_id
+}
+
+#
+# Create a Cluster
+#
+resource "mongodbatlas_cluster" "test" {
+  project_id   = mongodbatlas_project.my_project.id
+  name         = "test"
+  num_shards   = 1
+
+  replication_factor           = 3
+  backup_enabled               = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "4.0"
+
+  //Provider Settings "block"
+  provider_name               = "GCP"
+  disk_size_gb                = 40
+  provider_instance_size_name = "M30"
+  provider_region_name        = "US_EAST_4"
+}
+
+#
+# Create a Database User
+#
+resource "mongodbatlas_database_user" "test" {
+  username 		= "${var.database_username}"
+  password 	 	= "${var.database_user_password}"
+  project_id            = "${mongodbatlas_project.my_project.id}"
+  database_name	 	= "admin"
+
+  roles {
+    role_name     	= "readWriteAnyDatabase"
+    database_name 	= "admin"
+  }
+}
+
+#
+# Create an IP Whitelist
+#
+resource "mongodbatlas_project_ip_whitelist" "test" {
+  project_id            = "${mongodbatlas_project.my_project.id}"  
+  whitelist {  
+    ip_address 		= var.whitelist_ip
+    comment    		= var.whitelist_ip_desc
+  }
+}
+
